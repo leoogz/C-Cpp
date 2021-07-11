662c74df1aedb0cfb1149b73ded174c03a1c2df1
Young-Kyu Kang <wooridyty4@gmail.com>
2021-07-11 17:29:03 +0900

    Problem No.5086 solved

---
+++ Algorithm/BaekJoon/N5086.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+priority_queue<int> pq;
+
+int main()
+{
+    int a, b;
+    while (1)
+    {
+        scanf("%d %d", &a, &b);
+        if (a == 0 && b == 0)
+            break;
+        if (b % a == 0)
+        {
+            printf("factor\n");
+        }
+        else if (a % b == 0)
+        {
+            printf("multiple\n");
+        }
+        else
+        {
+            printf("neither\n");
+        }
+    }
+}
